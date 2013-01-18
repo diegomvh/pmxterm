37c91ed66a4acf9fdf8134ddebf2ff20192536e8
diegomvh <diegomvh@gmail.com>
2013-01-17 22:16:01 -0300

    tests

--- colors.py
+++ colors.py
@@ -3,9 +3,10 @@
 # Display the colors available in a terminal.
 
 print "16-color mode:"
-for color in range(0, 16) :
-    for i in range(0, 3) :
-        print "\033[1;%sm%02s\033[m" % (str(color + 30), str(color)),
+for color in range(0, 16):
+    print "\033[1;49m0\033[m",
+    for i in range(0, 20):
+        print "\033[%s;%sm%02s-%02s\033[m" % (str(i), str(color + 30), str(i), str(color)),
     print
 
 # Programs like ls and vim use the first 16 colors of the 256-color palette.
